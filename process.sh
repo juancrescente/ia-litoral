8b4b1578f344a2d47b76a6bf8bc4eb2b7f7972cd
juan <juan.crescente@gmail.com>
2019-11-15 21:58:51 -0300

    fotos fix

--- process.sh
+++ process.sh
@@ -2,7 +2,7 @@ echo '>> START : Audio to Spectrogram creation <<'
 
 for audioFile in ./data/data/*.wav; do
         echo $audioFile
-        ffmpeg -y -i $audioFile -lavfi showspectrumpic=s=800x400:legend=disabled $audioFile.jpg
+        ffmpeg -y -i $audioFile -lavfi showspectrumpic=s=600x300:legend=disabled $audioFile.jpg
     done
     
 echo '>> END : Audio to Spectrogram creation <<'
